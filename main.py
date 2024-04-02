a3320f794a01eb3dead8d49b1ce11e5c3d2f2eeb
Daniel Henrique <85520325+Dani701-32@users.noreply.github.com>
2024-04-02 09:11:07 -0300

    Class 4 Basics
    Making the graph just for study use

--- main.py
+++ main.py
@@ -1,84 +1,82 @@
-# Aula 2: Importação
+# Aula 4: Análises Avançadas de Ações e Gráficos de Velas
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.dates as mdates
+import mplfinance as mpf
+import yfinance as yf
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-file = "Cópia de Imersão Python - Tabela de ações.xlsx"
+data = yf.download("PETR4.SA", start="2023-01-01", end="2023-12-31")  # petrobras 2023
+data["Close"].plot(figsize=(10, 6))
+plt.title("Variation by date", fontsize=16)
+plt.legend(["Close"])
 
-df_principal = pd.read_excel(file, sheet_name="Principal")
-df_total_de_acoes = pd.read_excel(file, sheet_name="Total_de_acoes")
-df_ticker = pd.read_excel(file, sheet_name="Ticker")
-df_chatgpt = pd.read_excel(file, sheet_name="ChatGPT")
+df = data.head(60).copy()
+# Convertedo o indice em uma coluna de data
+df["Date"] = df.index
+# Convertendo as datas para o formato numérico de matplotlib
+# Isso é necessário para que o matplotlib possa plotar as datas corretamente no gráfico
+df["Date"] = df["Date"].apply(mdates.date2num)
+fig, ax = plt.subplots(figsize=(15, 8))
+# Vamos definir a largura dos candles no gráfico
+width = 0.7
 
-# Aula 03: Manipulação de Dados e Criação de Gráficos com bibliotecas Python
-df_principal = df_principal[["Ativo", "Data", "Último (R$)", "Var. Dia (%)"]].copy()
-df_principal = df_principal.rename(
-    columns={"Último (R$)": "last_value", "Var. Dia (%)": "var_day_percent"}
-).copy()
+for i in range(len(df)):
+    # Determinando a cor docandle
+    # Se o preço de fechamento for maior que o de abertura, o candle é verde
+    # se for menor, o candle é vermelho
+    if df["Close"].iloc[i] > df["Open"].iloc[i]:
+        color = "green"
+    else:
+        color = "red"
 
-df_principal["var_percent"] = df_principal["var_day_percent"] / 100
-
-df_principal["var_start"] = df_principal["last_value"] / (
-    df_principal["var_percent"] + 1
-)
-df_principal = df_principal.merge(
-    df_total_de_acoes, left_on="Ativo", right_on="Código", how="left"
-)
-
-df_principal = df_principal.drop(columns=["Código"])
-
-df_principal["var_rs"] = (
-    df_principal["last_value"] - df_principal["var_start"]
-) * df_principal["Qtde. Teórica"]
-
-pd.options.display.float_format = (
-    "{:.2f}".format
-)  # Formata os float removemnto a notação científica
-
-df_principal["Qtde. Teórica"] = df_principal["Qtde. Teórica"].astype(int)
-
-df_principal = df_principal.rename(columns={"Qtde. Teórica": "qtd_theory"}).copy()
-
-df_principal["result"] = df_principal["var_rs"].apply(
-    lambda x: "Subiu" if x > 0 else ("Desceu" if x < 0 else "Manteve")
-)
-
-df_principal = df_principal.merge(
-    df_ticker, left_on="Ativo", right_on="Ticker", how="left"
-)
-df_principal = df_principal.drop(columns=["Ticker"])
-
-df_principal = df_principal.merge(
-    df_chatgpt, left_on="Nome", right_on="Nome da Empresa", how="left"
-)
-df_principal = df_principal.drop(columns=["Nome da Empresa"])
-
-df_principal = df_principal.rename(columns={"Idade (em anos)": "age"}).copy()
-
-df_principal["age_detail"] = df_principal["age"].apply(
-    lambda x: (
-        "Mais de 100" if x > 100 else ("Menos de 50" if x < 50 else "Entre 100 e 50")
+    # Desenha a linha vetical do candle
+    # Essa linha mostra os preços máximo( topo da linha) e mínimo (base da linha) do dia.
+    # Usamos 'ax.plot' para desenhar uma linha vertical.
+    # [df["Data"].iloc[i], df["Data"].iloc[i]] define o ponto x da linha (a data) e [df["low"].iloc[i], df["High"].iloc[i]] define a altura do canlde
+    ax.plot(
+        [df["Date"].iloc[i], df["Date"].iloc[i]],
+        [df["Low"].iloc[i], df["High"].iloc[i]],
+        color=color,
+        linewidth=1,
     )
-)
+    ax.add_patch(
+        patches.Rectangle(
+            (
+                df["Date"].iloc[i] - width / 2,
+                min(df["Open"].iloc[i], df["Close"].iloc[i]),
+            ),
+            width,
+            abs(df["Close"].iloc[i] - df["Open"].iloc[i]),
+            facecolor=color,
+        )
+    )
+df["MA7"] = df["Close"].rolling(window=7).mean()
+df["MA14"] = df["Close"].rolling(window=14).mean()
 
-#Analises
+# Plotanto as médias móveis
+ax.plot(
+    df["Date"], df["MA7"], color="orange", label="Média Móvel 7 dias"
+)  # Média de 7 dias
+ax.plot(
+    df["Date"], df["MA14"], color="yellow", label="Média Móvel 14 dias"
+)  # Média de 14 dias
+# adicionar legendas para as médias
+ax.legend()
 
-maxValue = df_principal["var_rs"].max()
-minValue = df_principal["var_rs"].min()
-averageValue = df_principal["var_rs"].mean()
-averageUpValue = df_principal[df_principal["result"] == "Subiu"]["var_rs"].mean()
-averageDownValue = df_principal[df_principal["result"] == "Desceu"]["var_rs"].mean()
+# formatando o eixo x para mostrar as datas
+# Configuramos o formato da adata e a toração para melhor legibilidade
+ax.xaxis_date()  # O Método é usado para dizer ao matplotlib que as datas estão sendo usadas no eixo x
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+plt.xticks(rotation=45)
 
-print(f'Maior\tR$ {maxValue:,.2f}')
-print(f'Menor\tR$ {minValue:,.2f}')
-print(f'Média\tR$ {averageValue:,.2f}')
-print(f'Média de quem Subiu\tR$ {averageUpValue:,.2f}')
-print(f'Média de quem Desceu\tR$ {averageDownValue:,.2f}\n')
+#adicionando titulo e rotulos para os eixos x e y
+plt.title("Gráfico de candlestic - Petra.SA com matplotlib")
+plt.xlabel("Date")
+plt.ylabel("Price")
 
-df_principal_subiu = df_principal[df_principal["result"] == "Subiu"]
-
-df_analise_segment = df_principal_subiu.groupby('Segmento')['var_rs'].sum().reset_index()
-
-df_analise_saldo = df_principal.groupby('result')['var_rs'].sum().reset_index()
-
-fig = px.bar(df_analise_saldo, x="result", y='var_rs', text='var_rs', title='Variação Reais por Resultado')
-fig.show()
+#adicionando uma grade para facilitar a visiualização dos valores
+plt.grid(True)
+plt.show()
